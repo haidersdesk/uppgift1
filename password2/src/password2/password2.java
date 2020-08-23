cc3044dd5da44d349ca1ec340edbece057c62590
haidersdesk <67119669+haidersdesk@users.noreply.github.com>
2020-08-23 22:54:50 +0200

    Update password2.java

--- password2/src/password2/password2.java
+++ password2/src/password2/password2.java
@@ -8,7 +8,7 @@ public class password2 {
 
 		int i = 0;
 
-		while (i < 3) { // while loop som repeterar tills variabel i är like mindre än 3
+		while (i < 3) { // while loop som repeterar tills variabel i Ã¤r mindre Ã¤n 3
 
 			System.out.println(" :::::::PLEASE ENTER A VALID PINCODE :::::::");
 			Scanner reader = new Scanner(System.in);
@@ -17,14 +17,14 @@ public class password2 {
 				String userPass = reader.nextLine();
 				int userPin = Integer.parseInt(userPass); // omvandla string till integer med parseInt
 				System.out.println("Pincode accepted");
-				break; // avsluta loop om användaren matar in siffror
+				break; // avsluta loop om anvÃ¤ndaren matar in siffror
 			}
 
-			catch (Exception e) { // Om användaren matar in fel
+			catch (Exception e) { // Om anvÃ¤ndaren matar in fel
 				System.err.println("You can only enter numbers");
-				i++; // Variabel i höjs med +1
+				i++; // Variabel i hÃ¶js med +1
 
-				if (i == 3) // om variabel är lika med 3 då visas meddelandet att konton är avstängd
+				if (i == 3) // om variabel Ã¤r lika med 3 dÃ¥ visas meddelandet att konton Ã¤r avstÃ¤ngd
 				{
 					System.out.println("Sorry Your account is Locked");
 
